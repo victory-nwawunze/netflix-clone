8df173b8ef5985e90118f1de852ffb3263332682
victory-nwawunze <victorynwawunze@gmail.com>
2022-02-27 11:50:55 -0800

    still implementing javascript functionalities

--- js/main.js
+++ js/main.js
@@ -5,8 +5,18 @@ const tabContentItems = document.querySelectorAll(".tab-content-item");
 
 // select tab content item
 function selectItem(e) {
+  removeBorder();
+  removeShow();
   // Add border to current tab
   this.classList.add("tab-border");
+  //   Grab content item from DOM
+  const tabContentItem = document.querySelector(`#${this.id}`);
+}
+function removeBorder() {
+  tabItems.forEach((item) => item.classList.remove("tab-border"));
+}
+function removeShow() {
+  tabContentItems.forEach((item) => item.classList.remove("show"));
 }
 // Listen for tab click
 tabItems.forEach((item) => item.addEventListener("click", selectItem));
