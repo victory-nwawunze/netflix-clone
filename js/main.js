703a739cae34e39a6c0583fc1712db56ad21972a
victory-nwawunze <victorynwawunze@gmail.com>
2022-02-27 11:34:57 -0800

    Adding javascript functionalities

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/** @format */
+
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+
+// select tab content item
+function selectItem(e) {
+  // Add border to current tab
+  this.classList.add("tab-border");
+}
+// Listen for tab click
+tabItems.forEach((item) => item.addEventListener("click", selectItem));
